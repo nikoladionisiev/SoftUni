8455476407e5f1bea91a77fe3bbfeaeb2b746472
Nikola Dionisiev <nikola.dionisiev@gmail.com>
2021-03-11 19:43:20 +0200

    StudentSystem

---
+++ Entity Framework Core/10. Entity Relations/Entity Relations/StudentSystem/P01_StudentSystem/Data/Models/Homework.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Homework
+    {
+        [Key]
+        public int HomeworkId { get; set; }
+
+        public string Content { get; set; }
+
+        public ContentType ContentType { get; set; }
+
+        public DateTime SubmissionTime { get; set; }
+
+        //fk student
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+        //fk course
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Entity Framework Core/10. Entity Relations/Entity Relations/StudentSystem/P01_StudentSystem/Data/Models/Resource.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+
+        [Key]
+        public int ResourceId { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Column("Url", TypeName = "varchar(max)")]
+        public string Url { get; set; }
+
+        public ResourceType ResourceType { get; set; }
+
+        //foreign key
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
