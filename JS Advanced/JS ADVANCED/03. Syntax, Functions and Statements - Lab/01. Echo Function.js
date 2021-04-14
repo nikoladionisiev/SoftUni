3eea183d0faff81cb39a5a6a98d77a6ff22b52c3
Nikola Dionisiev <nikola.dionisiev@gmail.com>
2021-04-14 22:35:57 +0300

    finished lab tasks

--- JS Advanced/JS ADVANCED/03. Syntax, Functions and Statements - Lab/01. Echo Function.js
+++ JS Advanced/JS ADVANCED/03. Syntax, Functions and Statements - Lab/01. Echo Function.js
@@ -117,4 +117,23 @@ function solve(x)
 }
 
 //09. Aggregate Elements
+function solve(arr){
 
+    var sum = arr.reduce(function(a, b){
+        return a + b;
+    }, 0);
+
+    let concatNumbers = arr.join('');
+
+    let sumReverse = 0.0;
+    for (let i = 0; i < arr.length; i++) {
+        sumReverse += 1/arr[i];
+    }
+
+    console.log(sum);
+    console.log(sumReverse);
+    console.log(concatNumbers);
+    
+}
+
+solve([1, 2, 3]);
